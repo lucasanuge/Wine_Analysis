d718a8d6f034f725c10471ee77623b7f11eb97dd
Lucas Chandra <lucas.chandra@outlook.com>
2021-10-08 22:00:54 -0400

    conduct mult linear reg

--- data_analysis/wine_analysis.R
+++ data_analysis/wine_analysis.R
@@ -1,6 +1,9 @@
 # set up for multiple linear regression
 
-wine_data <- read_csv("resources/wine_data.csv")
+library(stats)
 
-lm(quality ~ density ,data=wine_data) #generate multiple linear regression model
 
+
+linear_model <- lm(quality ~ fxd_acid + density + vol_acid + cit_acid + sugar + chlorides + free_SO2 + total_SO2 + density + pH + sulphates + alcohol ,data=wine_data) #generate multiple linear regression model
+
+summary(linear_model)
